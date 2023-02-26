a503a1d4aa5bbbc3e30b6c15bd0ee6c15221e3fc
Your Name <vinny.purgato@gmail.com>
2023-02-26 10:21:12 -0500

    insert, max/min val

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -14,17 +14,26 @@ class BST:
     def insert(self, value):
         start = self.root
         new_node = tree_node(value)
+        acc = None
 
         while start != None:
+            acc = start
+
             if value > start.value:
                 start = start.right
             elif value < start.value:
                 start = start.left
-            else:
-                break
 
-            if start == None:
-                start = new_node
+            
+        if acc == None:
+            acc = new_node
+        elif (value < acc.value):
+            acc.left = new_node
+        else:
+            acc.right = new_node
+        
+        return acc
+
 
     def max_value(self):
         start = self.root
@@ -40,8 +49,30 @@ class BST:
             start = start.left
             if start.left == None:
                 acc = start
-        return acc.value        
+        return acc.value
+
+    def traversal(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+
+    def preorder_print(self, start, traversal):
+        if start != None:
+            traversal = traversal + str(start.value) + "-"
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+    def inorder_print(self, start, traversal):
+        if start != None:
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = traversal + str(start.value) + "-"
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal     
 
 tree = BST(10)
 tree.insert(30)
-print(tree.max_value)
+tree.insert(25)
+tree.insert(50)
+print(tree.max_value())
